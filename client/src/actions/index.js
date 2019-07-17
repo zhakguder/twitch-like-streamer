e57b3a3ea4a0b5b0c215c9c08c6aa01922e74941
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2019-07-17 03:46:43 -0500

    add modal

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
   DELETE_STREAM,
   EDIT_STREAM
 } from "./types";
+import history from "../history";
 
 export const signIn = userId => {
   return { type: SIGN_IN, payload: userId };
@@ -17,9 +18,12 @@ export const signOut = () => {
   return { type: SIGN_OUT };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+
+  const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -33,8 +37,9 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
-  dispatch({ type: EDIT_STREAM, payload: response.data });
+  const response = await streams.patch(`/streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+    history.push("/");
 };
 
 export const deleteStream = id => async dispatch => {
